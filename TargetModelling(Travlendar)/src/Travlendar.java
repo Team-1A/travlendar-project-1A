5c788349dd00f74dca54de32f78f7220ed2683cd
Fadhil226 <muhammad.fadhil.tif16@polban.ac.id>
2017-09-19 23:42:22 +0700

    perubahan
    menyamakan dengan tugas OOP dengan sistem input dan output

--- TargetModelling(Travlendar)/src/Travlendar.java
+++ TargetModelling(Travlendar)/src/Travlendar.java
@@ -1,4 +1,5 @@
 
+import static java.lang.System.exit;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -29,6 +30,38 @@ public class Travlendar {
         ArrayList<transportasi> listKendaraan = new ArrayList<>();
         transportasi kendaraan = new transportasi();
         
+        System.out.println("1. Tambahkan Acara");
+        System.out.println("2. Tampilkan list Acara");
+        System.out.println("3. Exit");
+        System.out.print("Pilihan: ");
+        int pilihan = Integer.parseInt(scan.nextLine());
+        do{
+            switch(pilihan){
+                case 1:{
+                    listKegiatan.add(Acara.masukanAcara());
+                    break;
+                }
+                case 2:{
+                    System.out.println("List acara");
+                    listAcara.forEach(Schedule -> System.out.println(Schedule.getAcara()));
+                    break;
+                }
+                case 3:{
+                    exit(0);
+                }
+                default:{
+                    System.out.println("Pilihan salah");
+                    break;
+                }
+            }
+            if(pilihan == 1 || pilihan == 2) {
+                System.out.println("1. Tambahkan Acara hari ini");
+                System.out.println("2. Tampilkan list Acara hari ini");
+                System.out.println("3. Exit");
+                System.out.print("Pilihan: ");
+                pilihan = Integer.parseInt(scan.nextLine());
+            }
+        }while(pilihan!=3);
     }
     
 }
