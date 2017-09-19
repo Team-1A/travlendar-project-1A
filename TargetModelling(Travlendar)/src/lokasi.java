890d6cc83c81e4ed1f1e0bbbf6afaa17162e11d4
rohmatD <dasukirohmat@gmail.com>
2017-09-19 22:43:37 +0700

    menambahkan class lokasi

--- TargetModelling(Travlendar)/src/lokasi.java
+++ TargetModelling(Travlendar)/src/lokasi.java
@@ -3,11 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author RohmatDasuki
  */
 class lokasi {
+    public String namalokasi;
+    public int GMT ;  
     
+    public void Namalokasi(String nmlokasi )
+    {
+        namalokasi = nmlokasi;
+    }
+    public void Gmt(int gmt)
+    {
+        GMT=gmt;
+    }
+    public String namalokasi(String nl)
+    {
+        return "nama lokasi "+nl;
+    }
+    public int GMT(int gmT )
+    {
+        return  +gmT ;
+    }
 }
