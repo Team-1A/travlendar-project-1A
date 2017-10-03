143da3262d9cd801e5aad651bad759bf27b6af7e
rohmat97 <dasukirohmat@gmail.com>
2017-10-03 08:49:01 +0700

    array menjadi list
    pengubahan array menjadi list dan menggabungkan beberapa list menjadi beberapa field untuk mempermudah pekerjaan

--- Java Class App/src/Transportation.java
+++ Java Class App/src/Transportation.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Transportation {
@@ -6,6 +9,12 @@ public class Transportation {
     String[] kec ={"60 KMpH","50 KMpH","5 KMpH","990 KMpH"," "};
     String[] umum={"Mobil","Motor"};
     String[] kecumum={"60 KMpH","40 KMpH"};
+     List<String> list1 = new ArrayList<String>(Arrays.asList(mode ));
+     List<String> list2 = new ArrayList<String>(Arrays.asList(kec ));
+     List<String> list3 = new ArrayList<String>(Arrays.asList(umum ));
+     List<String> list4 = new ArrayList<String>(Arrays.asList(kecumum ));
+     List<String> fieldPribadi = new ArrayList<String>() { { addAll(list1); addAll(list2); } };
+     List<String> fieldUmum = new ArrayList<String>() { { addAll(list3); addAll(list4); } };
     /*
         Constructor pada class Transportation
     */
