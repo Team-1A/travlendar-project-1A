8cc36c619ca66d42be6f67f6b1dd8d136015a748
rohmat97 <dasukirohmat@gmail.com>
2017-10-09 00:35:40 +0700

    pembuatan model baru versi 2
    baru menambahkan beberapa class saja dan belum sesuai mockup

---
+++ Java Class App vers 2/JavaApplication3/src/javaapplication3/Location.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Rohmat Dasuki
+ */
+public class Location {
+    private String nameLocation;
+    private String address;
+    String [] startPoint ={"Rumah,Fatmawati","Bandara Soekarno Hatta,Tangerang","Bandara Haluoleo,Kadia","Hotel X,Kadia"};
+    String [] Destination={"Bandara Soekarno Hatta,Tangerang","Bandar Haluoleo,Kadia","Hotel X,Kadia","Restoran,Poasia"};
+    double [] jarak = {40 , 2600 ,21 ,0.200};//satuan KiloMeter
+    List<String> listAsal = new ArrayList<String>(Arrays.asList(startPoint ));
+    List<String> listTujuan = new ArrayList<String>(Arrays.asList(Destination ));
+    //List<double> listJarak =  new ArrayList<double>(Arrays.asList(jarak)); 
+    List<String> field = new ArrayList<String>() { { addAll(listAsal); addAll(listTujuan); } };
+      /*
+        Getter dan Settter pada class Location
+    */
+    public Location(){
+        nameLocation = null;
+        address = null;
+    }
+    public void setLocationName(String locationName) {
+        this.nameLocation = locationName;
+    }
+    public void setLocation(String location, String address){
+        this.nameLocation = location;
+        this.address = address;
+    }
+     public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    } 
+    public String getLocationName() {
+        return nameLocation;
+    }
+   
+}
---
+++ Java Class App vers 2/JavaApplication3/src/javaapplication3/Transportation.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Rohmat Dasuki
+ */
+public class Transportation {
+    private String TransportationMode;
+    String [] mode={"Pesawat","Mobil","Motor","Sepeda","Jalan Kaki"} ;
+    double [] kec ={990.00 ,60.00 ,50.00,15.00,5.00}; //dalam satuan KMpH
+   //  List<String> list1 = new ArrayList<String>(Arrays.asList(mode ));
+     //List<String> list =new ArrayList<double>(Arrays.asList(kec ));
+     //List<String> fieldPribadi = new ArrayList<String>() { { addAll(list1); addAll(list2); } };
+     public Transportation(){
+        TransportationMode = null;
+    }
+    /*
+        Getter dan Setter pada class Transportation
+    */
+    public String getTransportationMode() {
+        return TransportationMode;
+    }
+    public void setTransportationMode(String transportationMode) {
+        this.TransportationMode = transportationMode;
+    }
+    /*
+        Method inputTransportation() berfungsi untuk melakukan input moda transportasi dan kecepatan rata-rata
+    */
+}
