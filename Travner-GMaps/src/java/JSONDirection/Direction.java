2fecba1869a6a0e7542a945156a4cabc5b248cb6
Fadhil226 <muhammad.fadhil.tif16@polban.ac.id>
2017-12-11 22:57:29 +0700

    half

---
+++ Travner-GMaps/src/java/JSONDirection/Direction.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Hydrolyze
+ */
+public class Direction {
+    private Routes[] routes;
+
+    /**
+     * @return the routes
+     */
+    public Routes[] getRoutes() {
+        return routes;
+    }
+
+    /**
+     * @param routes the routes to set
+     */
+    public void setRoutes(Routes[] routes) {
+        this.routes = routes;
+    }
+    
+}
